333f9c0689fdd1c9a16b405e5e79336d368cec9c
Pranith <pranithgeddapu@gmail.com>
2024-04-13 08:25:36 -0400

    Pranith Geddapu commit

---
+++ main.py
@@ -0,0 +1,38 @@
+state= 1
+
+
+def enc(inp):
+    encpa = " "
+    for d in inp:
+        encd = str((int(d) + 3) % 10)
+        encpa = encpa + encd
+
+    return encpa
+
+
+
+
+while state == 1:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    option = input("Please enter an option: ")
+
+    if option == "1":
+        inp = input("Please enter your password to encode: ")
+        enc(inp)
+
+        encda = enc(inp)
+        print ("Your password has been encoded and stored!")
+
+    if option == "3":
+
+        state = 0
+
+
+
+
+
